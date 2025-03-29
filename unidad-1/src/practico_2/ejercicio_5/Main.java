3c511fe2c9956787c9a040f7203d898d248de6b8
AgusCuculich <cuculichagustina@gmail.com>
2025-03-28 21:43:43 -0300

    Ejercicio 5 resuelto

--- unidad-1/src/practico_2/ejercicio_5/Main.java
+++ unidad-1/src/practico_2/ejercicio_5/Main.java
@@ -2,38 +2,24 @@ package practico_2.ejercicio_5;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {0, 2, 3, 4};
-        determinarIgualdad(arr, 0);
+        int[] arr = {1, 5, 3, 4};
+        System.out.println('\n' + "La igualdad fue hallada? " + determinarIgualdad(arr, 0));
     }
 
     public static boolean determinarIgualdad(int[] arr, int pos) {
-        if(pos>= 0) {
-            if(pos < arr.length) {
-                System.out.println(arr[pos]);
-                determinarIgualdad(arr, pos+1);
-                return arr[pos] == pos;
+        // Verificamos que la posición sea válida.
+        if(pos >= 0 && pos < arr.length) {
+            System.out.print(arr[pos]);
+            if(arr[pos] == pos) {
+                // Si se encuentra un elemento igual a su posición, la función se corta devolviendo true y se
+                // completan el resto de funciones pausadas en la pila.
+                return true;
             }
+            // Si no se encuentra la igualdad, la recursión continua.
+            return determinarIgualdad(arr, pos+1);
         }
+        // Devolvemos false en caso de recorrer todo el arreglo y no hallar una igualdad. Lo mismo sucede si
+        // la posición es inválida.
         return false;
     }
-
-    /*
-        public static boolean determinarIgualdad(int[] arr, int pos) {
-        // Caso base: si hemos recorrido todo el array, retornamos true
-        if (pos == arr.length) {
-            return true;
-        }
-
-        // Imprimir el valor actual del arreglo y la posición
-        System.out.println(arr[pos]);
-
-        // Verificamos si el valor en arr[pos] es igual a la posición
-        if (arr[pos] != pos) {
-            return false;  // Si hay una diferencia, retornamos false
-        }
-
-        // Llamada recursiva: continuamos con el siguiente índice
-        return determinarIgualdad(arr, pos + 1);
-    }
-     */
 }
