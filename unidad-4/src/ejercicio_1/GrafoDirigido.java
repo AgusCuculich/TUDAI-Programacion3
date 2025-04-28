3de36d0a1e0c49d64540e2413aa9665eb291fb34
AgusCuculich <cuculichagustina@gmail.com>
2025-04-28 01:21:31 -0300

    Se añaden funciones y comprobaciones

--- unidad-4/src/ejercicio_1/GrafoDirigido.java
+++ unidad-4/src/ejercicio_1/GrafoDirigido.java
@@ -1,6 +1,8 @@
 package ejercicio_1;
 
+import java.text.CollationElementIterator;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 
@@ -27,8 +29,11 @@ public class GrafoDirigido<T> implements Grafo<T> {
 
     @Override
     public void agregarArco(int verticeId1, int verticeId2, T etiqueta) {
-        Arco<T> nuevoArco = new Arco<>(verticeId1, verticeId2, etiqueta);
+        if (!contieneVertice(verticeId1) || !contieneVertice(verticeId2)) {
+            throw new IllegalArgumentException("Vertice no existente: " + verticeId1 + " o " + verticeId2);
+        }
         ArrayList<Arco<T>> target = this.arcosAdyacentes.get(verticeId1);
+        Arco<T> nuevoArco = new Arco<>(verticeId1, verticeId2, etiqueta);
         if(!target.contains(nuevoArco)) {
             target.add(nuevoArco);
             this.cantArcos++;
@@ -47,6 +52,9 @@ public class GrafoDirigido<T> implements Grafo<T> {
 
     @Override
     public boolean existeArco(int verticeId1, int verticeId2) {
+        if(!contieneVertice(verticeId1) || !contieneVertice(verticeId2)) {
+            return false;
+        }
         ArrayList<Arco<T>> arcos = this.arcosAdyacentes.get(verticeId1);
         for(Arco<T> arcoActual : arcos) {
             if(arcoActual.getVerticeDestino() == verticeId2) {
@@ -58,7 +66,15 @@ public class GrafoDirigido<T> implements Grafo<T> {
 
     @Override
     public Arco<T> obtenerArco(int verticeId1, int verticeId2) {
-        // TODO Auto-generated method stub
+        if(!contieneVertice(verticeId1) || !contieneVertice(verticeId2)) {
+            return null;
+        }
+        ArrayList<Arco<T>> adyacentes = this.arcosAdyacentes.get(verticeId1);
+        for(Arco<T> arco : adyacentes) {
+            if(arco.getVerticeDestino() == verticeId2) {
+                return arco;
+            }
+        }
         return null;
     }
 
@@ -75,12 +91,21 @@ public class GrafoDirigido<T> implements Grafo<T> {
     @Override
     public Iterator<Integer> obtenerVertices() {
         return this.arcosAdyacentes.keySet().iterator();
+        // En caso de que KeySet devuelva un Set vacío, el iterador que devuelva esta función seguirá siendo un iterador
+        // válido que no tiene elementos
     }
 
     @Override
     public Iterator<Integer> obtenerAdyacentes(int verticeId) {
-        // TODO Auto-generated method stub
-        return null;
+        if(!contieneVertice(verticeId)) {
+            return Collections.emptyIterator();
+        }
+        ArrayList<Integer> adyacentes = new ArrayList<>();
+        ArrayList<Arco<T>> arcos = this.arcosAdyacentes.get(verticeId);
+        for(Arco<T> arco : arcos) {
+            adyacentes.add(arco.getVerticeDestino());
+        }
+        return adyacentes.iterator();
     }
 
     @Override
@@ -90,10 +115,23 @@ public class GrafoDirigido<T> implements Grafo<T> {
             totalArcos.addAll(arcos);
         }
         return totalArcos.iterator();
+        // Si la lista existe (no es null), no necesitas comprobar si está vacía antes de llamar a iterator()
+        // El iterador de una lista vacía simplemente te dice que no hay elementos (hasNext() → false).
     }
 
     @Override
     public Iterator<Arco<T>> obtenerArcos(int verticeId) {
-        return this.arcosAdyacentes.get(verticeId).iterator();
+        if(!contieneVertice(verticeId)) {
+            return Collections.emptyIterator();
+        }
+        ArrayList<Arco<T>> arcos = this.arcosAdyacentes.get(verticeId);
+        return arcos.iterator();
+        // Collections.emptyIterator() te da un iterador que no tiene elementos.
+        // De esta manera no devolvemos un null que genere un error ante el sig caso:
+        // Iterator<Arco<T>> it = grafo.obtenerArcos(id);
+        //    while (it.hasNext()) {
+        //        Arco<T> arco = it.next();
+        //        …
+        //    }
     }
 }
