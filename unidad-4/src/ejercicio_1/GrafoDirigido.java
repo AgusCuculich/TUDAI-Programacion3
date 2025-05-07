12ee8ad9e72b121626ca4641dba1356cb150d261
AgusCuculich <cuculichagustina@gmail.com>
2025-05-07 01:01:52 -0300

    Recorrido DFS implementado

--- unidad-4/src/ejercicio_1/GrafoDirigido.java
+++ unidad-4/src/ejercicio_1/GrafoDirigido.java
@@ -1,17 +1,15 @@
 package ejercicio_1;
 
-import java.text.CollationElementIterator;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.*;
 
 public class GrafoDirigido<T> implements Grafo<T> {
     private HashMap<Integer, ArrayList<Arco<T>>> arcosAdyacentes;
+    private HashMap<Integer,Character> estadoVertices;
     private int cantArcos;
 
     public GrafoDirigido() {
         this.arcosAdyacentes = new HashMap<>();
+        this.estadoVertices = new HashMap<>();
         this.cantArcos = 0;
     }
 
@@ -21,6 +19,7 @@ public class GrafoDirigido<T> implements Grafo<T> {
         // con una lista vacía de arcos.
         if(!this.arcosAdyacentes.containsKey(verticeId)) {
             this.arcosAdyacentes.put(verticeId, new ArrayList<>());
+            this.estadoVertices.put(verticeId, 'B');
         }
     }
 
@@ -51,6 +50,7 @@ public class GrafoDirigido<T> implements Grafo<T> {
         this.cantArcos -= arcosSalientes.size();
         // Eliminamos definitivamente el vértice del grafo (y junto a él, su lista de arcos salientes).
         this.arcosAdyacentes.remove(verticeId);
+        this.estadoVertices.remove(verticeId)
     }
 
     @Override
@@ -197,5 +197,40 @@ public class GrafoDirigido<T> implements Grafo<T> {
         //        Arco<T> arco = it.next();
         //        …
         //    }
+    }
+
+    public void DFS(){
+        //Al modificar las entries, se modifican tambien los valores del hashmap original
+        // (el set tiene las mismas referencias que el mapa)
+        Set<Map.Entry<Integer, Character>> entries = this.estadoVertices.entrySet();
+
+        //Para cada vértice del mapa de estados, los pintamos de blanco
+        for(Map.Entry<Integer,Character> entry : entries){
+            entry.setValue('B');
+        }
+
+        for(Map.Entry<Integer,Character> entry : entries){
+            //Si el vértice esta sin visitar (blanco)
+            if (entry.getValue().equals('B')){
+                DFS_Visit(entry.getKey());
+            }
+        }
+    }
+
+    public void DFS_Visit(Integer verticeId){
+        //Marcamos el vértice como visitado (amarillo)
+        this.estadoVertices.put(verticeId,'A');
+        // Creamos un iterador de todos los vertices adyacentes del que estamos posicionados
+        Iterator<Integer> itVerticesAd = this.obtenerAdyacentes(verticeId);
+        while(itVerticesAd.hasNext()) {
+            // Guardamos el vértice y pasamos al sig
+            Integer verticeAd = itVerticesAd.next();
+            // Si este vértice adyacente esta blanco, recorremos sus adyacentes
+            if(this.estadoVertices.get(verticeAd).equals('B')) {
+                DFS_Visit(verticeAd);
+            }
+        }
+        // Una vez que recorrimos todos los adyecentes de este vértice, lo pintamos de negro
+        this.estadoVertices.put(verticeId, 'N');
     }
 }
